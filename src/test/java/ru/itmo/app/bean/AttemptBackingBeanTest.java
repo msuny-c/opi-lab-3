fea86ef47d7208a6253155bf8844179fc2e2180a
Munir <munirbairamgulov@gmail.com>
2025-04-21 00:26:12 +0300

    feat: initial commit

---
+++ src/test/java/ru/itmo/app/bean/AttemptBackingBeanTest.java
@@ -0,0 +1,52 @@
+package ru.itmo.app.bean;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AttemptBackingBeanTest {
+
+    private AttemptBackingBean bean;
+
+    @Before
+    public void setUp() {
+        bean = new AttemptBackingBean();
+    }
+
+    @Test
+    public void testDefaultValues() {
+        // Default values should be initialized as defined in the class
+        assertEquals(0.0, bean.getX(), 0.001);
+        assertEquals(0.0, bean.getY(), 0.001);
+        assertEquals(0.0, bean.getR(), 0.001);
+        assertNull(bean.getType());
+    }
+
+    @Test
+    public void testSetAndGetX() {
+        double x = 2.0;
+        bean.setX(x);
+        assertEquals(x, bean.getX(), 0.001);
+    }
+
+    @Test
+    public void testSetAndGetY() {
+        double y = 3.0;
+        bean.setY(y);
+        assertEquals(y, bean.getY(), 0.001);
+    }
+
+    @Test
+    public void testSetAndGetR() {
+        double r = 1.0;
+        bean.setR(r);
+        assertEquals(r, bean.getR(), 0.001);
+    }
+
+    @Test
+    public void testSetAndGetType() {
+        String type = "form";
+        bean.setType(type);
+        assertEquals(type, bean.getType());
+    }
+} 
---
+++ src/test/java/ru/itmo/app/validation/ScaleValidatorTest.java
@@ -0,0 +1,59 @@
+package ru.itmo.app.validation;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class ScaleValidatorTest {
+
+    private ScaleValidator validator;
+    private Scale annotation;
+    private ConstraintValidatorContext context;
+
+    @Before
+    public void setUp() {
+        validator = new ScaleValidator();
+        annotation = new Scale() {
+            @Override
+            public Class<?>[] groups() {
+                return new Class[0];
+            }
+
+            @Override
+            public String message() {
+                return "{validation.scale}";
+            }
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public Class<? extends jakarta.validation.Payload>[] payload() {
+                return new Class[0];
+            }
+
+            @Override
+            public int value() {
+                return 1;
+            }
+
+            @Override
+            public Class<java.lang.annotation.Annotation> annotationType() {
+                return java.lang.annotation.Annotation.class;
+            }
+        };
+        validator.initialize(annotation);
+        context = null; // Not needed for testing
+    }
+
+    @Test
+    public void testValidScale() {
+        // Double with one decimal place should be valid
+        assertTrue(validator.isValid(1.0, context));
+    }
+
+    @Test
+    public void testInvalidScale() {
+        // Double with two decimal places should be invalid
+        assertFalse(validator.isValid(1.23, context));
+    }
+} 
